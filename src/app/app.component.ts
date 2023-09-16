f2864a164bf3b80252f7fd0d3df544bab324e0e1
navjosingh1 <136000843+navjosingh1@users.noreply.github.com>
2023-09-16 13:50:11 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 import { GeneralConstant } from '../assets/constant';
 
 @Component({
@@ -8,9 +8,4 @@ import { GeneralConstant } from '../assets/constant';
 })
 export class AppComponent {
   title = GeneralConstant.FOOTBALL_TITLE;
-
-  @HostListener("window:onbeforeunload",["$event"])
-    clearLocalStorage(event){
-        localStorage.clear();
-  }
 }
