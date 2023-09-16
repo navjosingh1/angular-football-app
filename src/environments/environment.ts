1621ff0917e70c46d5ab979d5e42779e19a527a0
navjosingh1 <136000843+navjosingh1@users.noreply.github.com>
2023-09-16 14:20:56 +0530

    1 file modified

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,9 +5,6 @@
 export const environment = {
   production: false,
   API_HOST_URL: 'https://v3.football.api-sports.io',
-  //API_KEY: 'caa962e3a3bfca6c8da9f2662a8c495d',
-  //API_KEY: '43c4e4624ec5e731286a2a7308cd88e5',
-  //API_KEY: '4afef8eb8694a72b399c11bc46f81452',
   API_KEY: '5b16c4337a7453c4788557fc56cf53b8',
 };
 
